8700c116441be5c14a2abe3b821786aadf3bc177
Jorge <jg4@iastate.edu>
2024-08-12 19:28:20 -0500

    add sample

---
+++ device_mem_info_module.F90
@@ -0,0 +1,40 @@
+module device_mem_info_module
+  use, intrinsic :: iso_c_binding
+  implicit none
+
+interface 
+#ifdef HAVE_CUDA
+        integer (c_int) function cudaMemGetInfo(fre, tot) &
+                        bind(C, name="cudaMemGetInfo")
+            use iso_c_binding
+            implicit none
+            type(c_ptr),value :: fre
+            type(c_ptr),value :: tot
+        end function cudaMemGetInfo
+#elif HAVE_HIP
+    integer(c_int) function hipMemGetInfo(free, total) bind(C, name="hipMemGetInfo")
+      type(c_ptr), value :: free, total
+    end function hipMemGetInfo
+#endif
+end interface
+
+contains
+    ! Implementation of the fail function
+    integer(c_int) function fail() result(error)
+        implicit none
+        print *, "SUPER FAIL"
+        error = 69
+    end function fail
+
+    integer(c_int) function get_gpu_mem_info(free, total) result(error) 
+      type(c_ptr), value :: free, total 
+#ifdef HAVE_CUDA 
+      error = cudaMemGetInfo(free,total)
+#elif HAVE_HIP
+      error = hipMemGetInfo(free,total)
+#else
+      error = fail()
+#endif
+    end function 
+
+end module device_mem_info_module
---
+++ main.f90
@@ -0,0 +1,25 @@
+
+program main
+use device_mem_info_module
+  use iso_c_binding
+  implicit none
+      type(c_ptr) :: cpfree, cptot
+    integer*8, target :: free, total
+    integer res
+
+    cpfree = c_loc(free)
+    cptot = c_loc(total)
+
+    res = get_gpu_mem_info(cpfree, cptot)
+!    res = cudaMemGetInfo(cpfree, cptot)
+    print *, res
+    if(res.eq.0) then 
+    write(*,*)"GPU free mem:", free
+    write(*,*)" "
+    else
+    write(*,*) "FAILL"
+    endif
+
+
+
+end program main
